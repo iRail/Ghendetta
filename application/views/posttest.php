17c889a6d9d1a222280a3e83987983dc0d69b731
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2012-04-28 17:52:32 +0200

    up-to-date clan branch

---
+++ application/views/posttest.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <form action="http://ghendetta/index.php/foursquare/push" method="POST">
+        <textarea rows="10" cols="30" name="checkin">{"id":"test","user":{"id":"2506010"},"createdAt":"1333905591","venue":{"categories":[{"id":"4bf58dd8d48988d100941735"}],"id":"4b23cdf0f964a5201c5a24e3","location":{"lat":51.043892,"lng":3.713638}}}</textarea><br />
+        <input type="text" name="secret" value="secret"/>
+        <input type="submit" value="submit" />
+    </form>
+</body>
+    
+</html>
